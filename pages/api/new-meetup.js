25c4840834fe7a282941ff3974c275a6ce60c5a3
Shahrukh <shahrukh1494@gmail.com>
2023-10-09 21:04:05 +0530

    fixed mongodb auth issue

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://shahrukh:8rAHhp0hDm9rxHuM@cluster0.lz28xro.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://shahrukh:AlFH6FqJV6XvCKQh@cluster0.lz28xro.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
 
--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ function HomePage(props) {
 export async function getStaticProps() {
   // fetch data from an API
   const client = await MongoClient.connect(
-    "mongodb+srv://shahrukh:8rAHhp0hDm9rxHuM@cluster0.lz28xro.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://shahrukh:AlFH6FqJV6XvCKQh@cluster0.lz28xro.mongodb.net/meetups?retryWrites=true&w=majority"
   );
   const db = client.db();
 
